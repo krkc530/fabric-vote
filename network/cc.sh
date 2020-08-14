a6f1e3660e5f2a42863ebe3bf62c073e81b37beb
freeman <krkc530@naver.com>
2020-08-15 02:20:02 +0900

    new commit

---
+++ network/cc.sh
@@ -0,0 +1,23 @@
+CC_SRC_PATH=github.com/fabcar
+CHANNEL_NAME=mychannel
+CCNAME=fabcar
+VERSION=1.0
+PEER1=peer0.org1.example.com:7051
+PEER2=peer0.org2.example.com:7051
+PEER3=peer0.org3.example.com:7051
+
+#chaincode install
+docker exec cli peer chaincode install -n $CCNAME -v $VERSION -p ${CC_SRC_PATH}
+docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" \
+-e "CORE_PEER_ADDRESS=$PEER2" cli peer chaincode install -n $CCNAME -v $VERSION -p ${CC_SRC_PATH}
+docker exec -e "CORE_PEER_LOCALMSPID=Org3MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/crypto/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp" \
+-e "CORE_PEER_ADDRESS=$PEER3" cli peer chaincode install -n $CCNAME -v $VERSION -p ${CC_SRC_PATH}
+
+#chaincode instantiate
+docker exec cli peer chaincode instantiate -o orderer.example.com:7050 -C $CHANNEL_NAME -n $CCNAME -v $VERSION -c '{"Args":[]}' -P "AND ('Org1MSP.member','Org2MSP.member','Org3MSP.member')"
+sleep 3
+
+#chaincode test
+docker exec cli peer chaincode invoke -C $CHANNEL_NAME -n $CCNAME -c '{"Args":["initLedger"]}' --peerAddresses $PEER1 --peerAddresses $PEER2 --peerAddresses $PEER3
+sleep 3
+docker exec cli peer chaincode query -C $CHANNEL_NAME -n $CCNAME -c '{"Args":["queryAllCars"]}' 
